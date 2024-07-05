0981b016580961bef80d993921ed816a47a7fc88
lithiya <lithiyathomas12@gmail.com>
2024-07-05 17:25:22 +0530

    added rating

--- user_panel/urls.py
+++ user_panel/urls.py
@@ -6,5 +6,6 @@ app_name = 'user_panel'
 urlpatterns = [
     path('products/', UserPanelProductListView.as_view(), name='user_products'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
-    path('add_rating/', AddRatingView.as_view(), name='add_rating'),
+    path('product/<int:pk>/rate/', AddRatingView.as_view(), name='add_rating'),
 ]
+
